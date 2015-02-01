df713f1972778c7a1dc36672b6008e108359f80d
umezaki <bungoume3@yahoo.co.jp>
2015-02-01 23:25:29 +0900

    remove examples

--- webpayment_sample/webpayment_sample/urls.py
+++ webpayment_sample/webpayment_sample/urls.py
@@ -3,9 +3,6 @@ from django.contrib import admin
 
 urlpatterns = patterns(
     '',
-    # Examples:
-    # url(r'^$', 'webpayment_sample.views.home', name='home'),
-    # url(r'^blog/', include('blog.urls')),
     url(r'^', include('webfront.urls')),
     url(r'^admin/', include(admin.site.urls)),
     url(r'^payments/', include('payments.urls')),
